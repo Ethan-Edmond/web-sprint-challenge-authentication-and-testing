bff14688f2a9e37657ca574d4636a03a2fb0946f
Ethan Edmond <eedmond@uccs.edu>
2021-07-23 12:16:48 -0600

    npm i jsonwebtoken

--- api/server.test.js
+++ api/server.test.js
@@ -1,7 +1,9 @@
 // Write your tests here
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+
 const db = require('../data/dbConfig');
 const server = require('./server');
-const request = require('supertest');
 
 test('sanity', () => {
   expect(true).toBe(true);
